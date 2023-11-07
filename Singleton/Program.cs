aa2141fd8e32dc9b91e954c80bcde92bcd53ad7f
alibozlak <ali-bozlak@outlook.com>
2023-11-07 18:07:08 +0300

    Added Singleton Pattern

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -4,6 +4,46 @@
     {
         static void Main(string[] args)
         {
+            CustomerManager customerManager = CustomerManager.CraeteSingletonInstance();
+
+            Customer customer1 = new Customer("Ali Bozlak");
+
+            customerManager.Save(customer1);
+        }
+    }
+
+    class CustomerManager
+    {
+        private static CustomerManager _customerManager;
+
+        private CustomerManager()
+        {
+
+        }
+
+        public static CustomerManager CraeteSingletonInstance()
+        {
+            if (_customerManager == null)
+            {
+                _customerManager = new CustomerManager();
+            }
+            return _customerManager;
+        }
+
+        public void Save(Customer customer)
+        {
+            Console.WriteLine($"{customer.CustomerName} isimli müşteri veritabanına kaydedildi !!");
+        }
+    }
+
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+
+        public Customer(string customerName)
+        {
+            CustomerName = customerName;
         }
     }
 }
