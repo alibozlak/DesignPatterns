e8127e54509bc94592d526ebdc9a6f89f51fe233
alibozlak <ali-bozlak@outlook.com>
2023-12-30 20:33:29 +0300

    Added thread safe singleton pattern

---
+++ TreadSafeSingleton/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ThreadSafeSingleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    public class CustomerManager
+    {
+        private static CustomerManager _customerManager;
+        static object _lockObject = new object();
+
+        private CustomerManager() { }
+
+        public static CustomerManager CreateAsSingleton()
+        {
+            lock (_lockObject)
+            {
+                if (_customerManager == null)
+                {
+                    _customerManager = new CustomerManager();
+                }
+            }
+            return _customerManager;
+        }
+
+
+        // CustomerManager methods..
+    }
+}
