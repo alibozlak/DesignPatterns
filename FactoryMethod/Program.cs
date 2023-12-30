6cc6ab9051b6e09426be19e0556fc3dd3753bd43
alibozlak <ali-bozlak@outlook.com>
2023-12-30 21:06:37 +0300

    Added Factory Method Design Pattern

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace FactoryMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(new LoggerFactory());
+            customerManager.Save();
+        }
+    }
+
+    public class LoggerFactory : ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            return new EnginDemirogLogger();
+        }
+    }
+
+    public interface ILoggerFactory
+    {
+        ILogger CreateLogger();
+    }
+
+    public interface ILogger
+    {
+        void Log();
+    }
+
+    public class EnginDemirogLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with EnginDemirogLogger!");
+        }
+    }
+
+    public class CustomerManager
+    {
+        private ILoggerFactory _loggerFactory;
+
+        public CustomerManager(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Müşteri veritabanına kaydedildi!");
+            ILogger logger = _loggerFactory.CreateLogger();
+            logger.Log();
+        }
+    }
+}
