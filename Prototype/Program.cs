2d22cb04a70ae0ce3488de2ebabb2ba3f3c56492
alibozlak <ali-bozlak@outlook.com>
2024-01-07 11:40:30 +0300

    Added Prototype Design

---
+++ Prototype/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Prototype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee
+            {
+                Id = 1,
+                FirstName = "Engin",
+                LastName = "Demiroğ",
+                Salay = 78000
+            };
+
+            Employee employee2 = (Employee) employee1.Clone();
+            employee2.FirstName = "Salih";
+
+            Console.WriteLine(employee1.FirstName);
+            Console.WriteLine(employee2.FirstName);
+        }
+    }
+
+    public abstract class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName{ get; set; }
+
+        public abstract Person Clone();
+    }
+
+    public class Employee : Person
+    {
+        public decimal Salay { get; set; }
+
+        public override Person Clone()
+        {
+            return (Person) MemberwiseClone();
+        }
+    }
+}
